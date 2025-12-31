aa20c4b178de8fe0ee6f81d2185b4b89d31ee2d5
ldc <liudongcai@addcn.com>
2025-12-31 14:09:48 +0800

    feat：test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bb216c9624ed36e04392b03fddb64867",
+const RESOURCES = {"flutter_bootstrap.js": "c43a188794d96af8ac0694d27ca7a44c",
 "version.json": "b805adcc267ea434c7ee31fbdbab81c7",
 "index.html": "fb8f86ab9e5541596503624b7e7facd8",
 "/": "fb8f86ab9e5541596503624b7e7facd8",
-"main.dart.js": "c3bac6efa91694bc97025e05c3f69012",
+"main.dart.js": "06f1164f989b64b2b8f3f7876dadacc8",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
